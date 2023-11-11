ebe851f1fb9cf373b38196a527fea9e27c4971bf
leesangjun <hubls@naver.com>
2023-11-11 17:33:31 +0900

    feat: 중복 메뉴를 입력한 경우 검증 기능

---
+++ src/main/java/christmas/util/MenuType.java
@@ -0,0 +1,35 @@
+package christmas.util;
+
+public enum MenuType {
+    APPETIZER_PINE_MUSHROOM_SOUP("양송이수프", 6_000),
+    APPETIZER_TAPAS("타파스", 5_500),
+    APPETIZER_CAESAR_SALAD("시저샐러드", 8_000),
+
+    MAIN_T_BONE_STEAK("티본스테이크", 55_000),
+    MAIN_BBQ_RIBS("바비큐립", 54_000),
+    MAIN_SEAFOOD_PASTA("해산물파스타", 35_000),
+    MAIN_CHRISTMAS_PASTA("크리스마스파스타", 25_000),
+
+    DESSERT_CHOCO_CAKE("초코케이크", 15_000),
+    DESSERT_ICE_CREAM("아이스크림", 50_00),
+
+    BEVERAGE_ZERO_COLA("제로콜라", 3_000),
+    BEVERAGE_RED_WINE("레드와인", 60_000),
+    BEVERAGE_CHAMPAGNE("샴페인", 25_000);
+
+    private final String menuName;
+    private final int price;
+
+    MenuType(String menuName, int price) {
+        this.menuName = menuName;
+        this.price = price;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
--- src/main/java/christmas/util/OrdersValidator.java
+++ src/main/java/christmas/util/OrdersValidator.java
@@ -1,25 +1,51 @@
 package christmas.util;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class OrdersValidator {
+    private static final String ORDERS_REGEX = "^([가-힣A-Za-z]+)-([0-9]+)(?:,([가-힣A-Za-z]+)-([0-9]+))*$";
+    private static final String ORDERS_DUPLICATE_REGEX = "([\\w가-힣]+)-(\\d+)";
+    private static final int ORDER_MENU_NAME = 1;
+    private static final int ORDER_NUMBER_OF_MENU = 2;
 
     public static void validateOrders(String ordersText) {
         validateOrderFormat(ordersText);
+        validateDuplicate(ordersText);
     }
 
     private static void validateOrderFormat(String ordersText) {
-        // <문자열1>-<숫자1>,<문자열2>-<숫자2>,...,<문자열n>-<숫자n> 검증
-        Pattern pattern = Pattern.compile("^([가-힣A-Za-z]+)-([0-9]+)(?:,([가-힣A-Za-z]+)-([0-9]+))*$");
+        Pattern pattern = Pattern.compile(ORDERS_REGEX);
         Matcher matcher = pattern.matcher(ordersText);
         if (!matcher.matches()) {
             throw new IllegalArgumentException();
         }
     }
 
+    private static void validateDuplicate(String ordersText) {
+        List<String> menus = getMenuNames();
+        Set<String> uniqueMenuNames = new HashSet<>();
 
+        Pattern pattern = Pattern.compile(ORDERS_DUPLICATE_REGEX);
+        Matcher matcher = pattern.matcher(ordersText);
+
+        while (matcher.find()) {
+            String menuName = matcher.group(ORDER_MENU_NAME);
+
+            if (!uniqueMenuNames.add(menuName) || !menus.contains(menuName)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private static List<String> getMenuNames() {
+        List<String> menuNames = new ArrayList<>();
+
+        for (MenuType menuType : MenuType.values()) {
+            menuNames.add(menuType.getMenuName());
+        }
+
+        return menuNames;
+    }
 }
